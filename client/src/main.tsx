6420bbb87995cbfc6597b6fe7190ea425c7eacdd
Esteban Perez <20256524+idkwhttoput@users.noreply.github.com>
2025-05-10 12:32:23 -0400

    Delete client/src directory

--- client/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-document.title = "John Doe | Portfolio";
-createRoot(document.getElementById("root")!).render(<App />);
